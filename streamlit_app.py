dc7bba216c5b0b0f9a02862fbf119d71f205c67a
shubhamrockstar2433 <shubhamgupta2433@gmail.com>
2026-01-22 19:34:18 +0530

    Add import for active Snowflake session [ get_active_session]

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col,when_matched
+from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
