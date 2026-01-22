4f03683395a97be22808ffe3c57460ad2d7ba073
shubhamrockstar2433 <shubhamgupta2433@gmail.com>
2026-01-22 20:27:26 +0530

    Fix smoothie ordering logic and remove duplicate API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
         
     name_on_order = title
 
@@ -49,6 +51,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon='✅')
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
+
 
