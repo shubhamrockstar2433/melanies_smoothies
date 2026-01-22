dd419da24b27bf39aa3c25a68b42a0926114a09d
shubhamrockstar2433 <shubhamgupta2433@gmail.com>
2026-01-22 17:17:17 +0530

    added name_on_order

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #
+        
+    name_on_order = title
 
     my_insert_stmt = """
         insert into smoothies.public.orders (ingredients, name_on_order)
